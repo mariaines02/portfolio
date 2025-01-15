688b79e9d8111108f2221eae63c6a8af6733c99a
mariaines02 <mariahaddad@hotmail.fr>
2025-01-15 14:15:20 +0100

    Fix bugs

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Skill from "./components/Skill";
 import Work from "./components/Work";
-import Contact from "./components/contact";
+import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
 const App = () => {
